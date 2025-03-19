e2f28c4ce2a908d589efe1d0eaeafbc363ca22e0
Matharrr <matharrizqi@gmail.com>
2025-03-20 01:32:54 +0700

    first commit

---
+++ book-service/src/main/java/com/mr/book_service/BookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mr.book_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+}
